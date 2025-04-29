76efa36ff04660f47d6189bee21c7174b841f660
namddanh <namddanh@gmail.com>
2025-04-29 12:31:40 -0400

    basic boilerplate code added, removed default css

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
